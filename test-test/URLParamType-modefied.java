1094a027ea217073142132870a986c81ece6bf9f
Qichao Lan <lanqichao@gmail.com>
2022-03-17 22:29:13 -0700

    test data

---
+++ test-test/URLParamType-modefied.java
@@ -0,0 +1,76 @@
+/*
+ *  Copyright 2009-2016 Weibo, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.weibo.api.motan.common;
+
+import com.weibo.api.motan.config.RegistryConfig.Excise;
+
+/**
+ * @author maijunsheng
+ * @version 创建时间：2013-5-21
+ */
+public enum URLParamType {
+
+    private String name;
+    private String value;
+    private long longValue;
+    private int intValue;
+    private boolean boolValue;
+
+    URLParamType(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    URLParamType(String name, long longValue) {
+        this.name = name;
+        this.value = String.valueOf(longValue);
+        this.longValue = longValue;
+    }
+
+    URLParamType(String name, int intValue) {
+        this.name = name;
+        this.value = String.valueOf(intValue);
+        this.intValue = intValue;
+    }
+
+    URLParamType(String name, boolean boolValue) {
+        this.name = name;
+        this.value = String.valueOf(boolValue);
+        this.boolValue = boolValue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    public long getLongValue() {
+        return longValue;
+    }
+
+    public boolean getBooleanValue() {
+        return boolValue;
+    }
+
+}
